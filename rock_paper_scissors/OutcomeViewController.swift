6709ead27ed9f0cf738f9131deaa2a844c91c869
Anna Rogers <2muddybarefeet@gmail.com>
2016-06-30 07:25:29 -0700

    add rock turn, working on paper

---
+++ rock_paper_scissors/OutcomeViewController.swift
@@ -0,0 +1,66 @@
+//
+//  OutcomeViewController.swift
+//  rock_paper_scissors
+//
+//  Created by Anna Rogers on 6/30/16.
+//  Copyright © 2016 Anna Rogers. All rights reserved.
+//
+
+import UIKit
+
+class OutcomeViewController: UIViewController {
+    
+    var userTurn: Int?
+    var computerTurn: Int?
+    
+    @IBOutlet weak var resultsImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("reached the second view controller!", userTurn, computerTurn)
+//        pass the choices to find out who won
+//        unwrap the optional values else do not show anything!
+        if let computerGo = computerTurn {
+            if let userGo = userTurn {
+                let findImage = findWhoWon(computerGo, user: userGo)
+                resultsImage.image = UIImage(named:findImage)
+            }
+        } else {
+           resultsImage.image = nil
+        }
+    }
+    
+    //    function that gets passed the computersa and users go and then returns who appropriate image reference - trigger on load?
+    func findWhoWon (computer: Int, user: Int) -> String {
+        print("find who won function", computer, user)
+        if computer == user {
+//            then it is a draw
+            return "draw"
+        } else if user == 1 && computer == 2 {
+//        user has 1(rock) comp 2(paper) user loose
+            return "rock-paper"
+        } else if user == 1 && computer == 3 {
+//        user 1 rock comp 3 scissors user win
+            return "rock-scissors"
+        } else if user == 2 && computer == 1 {
+//        user has 2 paper comp 1 rock user win
+            return "rock-paper"
+        } else if user == 3 && computer == 1 {
+//        user 3 scissors comp has 1 rock user loose
+            return "rock-scissors"
+        } else if user == 2 && computer == 3 {
+//            user 2 paper and comp 3 scissors user loose
+            return "paper-scissors"
+        } else {
+//            user 3 scissors and computer 2  paper user win
+            return "paper-scissors"
+        }
+    }
+    
+//return to main page to play again
+    @IBAction func playAgain(sender: AnyObject) {
+        print("return to main page")
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+}
--- rock_paper_scissors/ViewController.swift
+++ rock_paper_scissors/ViewController.swift
@@ -10,16 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rock: UIButton!
+    @IBOutlet weak var scissors: UIButton!
+    @IBOutlet weak var paper: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("loading .....")
+        let a = genComputerTurn()
+        print("user turn", a)
+        rock.tag = 1
+        paper.tag = 2
+        scissors.tag = 3
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        print("in add data section", sender)
+        if segue.identifier == "scissorsChoice" {
+            var controller = segue.destinationViewController as! OutcomeViewController
+//            controller.userTurn = sender.tag
+            controller.computerTurn = genComputerTurn()
+        }
     }
-
-
+    
+    func genComputerTurn () -> Int {
+        let randomNumber = 1 + arc4random() % 3;
+        return Int(randomNumber)
+    }
+    
+    @IBAction func userTurn(sender: AnyObject) {
+        print("clicked", sender)
+        if sender.tag == 2 {
+            print("scissors chosen")
+            performSegueWithIdentifier("scissorsChoice", sender: self)
+        }
+        if sender.tag == 1 {
+            print("rock chosen")
+            var controllerCode:OutcomeViewController
+            controllerCode = storyboard?.instantiateViewControllerWithIdentifier("OutcomeViewController") as! OutcomeViewController
+            controllerCode.userTurn = sender.tag
+            controllerCode.computerTurn = genComputerTurn()
+            
+            presentViewController(controllerCode, animated: true, completion: nil)
+        }
+    }
+    
 }
 
